9d60ad8166da5eb7ca708c834e91b6e3b62754cf
Hari <malamharid86@gmail.com>
2022-11-16 16:54:18 +0530

    Update Queue Program

--- Insersion_Deletion_Que.c
+++ Insersion_Deletion_Que.c
@@ -1,62 +1,86 @@
 #include <stdio.h>
 int main()
 {
-    int que[10], size, rear;
+
+    // For Creating Queue
+    int que[10], size, rear = -1, front = -1, data;
     printf("Define Size: ");
     scanf("%d", &size);
-    printf("Define Initial rear: ");
-    scanf("%d", &rear);
-    int pointer;
-    for (pointer = 0; pointer < rear; pointer++)
+    int n;
+    printf("How Many Elements You Want To Enter: ");
+    scanf("%d", &n);
+    int i;
+    for (i = 0; i < n; i++)
     {
-        printf("\nEnter a Value in Que in position [%d]: ", pointer);
-        scanf("%d", &que[pointer]);
-    }
-    printf("Que Before Inserting Data:\n");
-    for (pointer = 0; pointer < rear; pointer++)
-    {
-        printf("%d ", que[pointer]);
+        rear++;
+        printf("\nEnter a Value in Que in position [%d]: ", i);
+        scanf("%d", &que[i]);
+        front = 0;
     }
 
-    int data;
+    // For Printing Queue
+    printf("Que Before Any Operation in Queue:\n");
+    for (i = 0; i <= rear; i++)
+    {
+        printf("%d ", que[i]);
+    }
+
+    // For Switch Case
     int y;
     printf("\n\nEnter 1 : For Insertion\nEnter 2 : For Deletion\n");
-    scanf("%d",&y);
+    scanf("%d", &y);
+
     switch (y)
     {
+
+    // Case 1 For Insertion In Queue
     case 1:
-        printf("\n\nEnter a Data You Want To Enter: ");
-        scanf("%d", &data);
-        if (rear <= size)
+
+        if (rear <= size - 1)
         {
+            printf("\n\nEnter a Data You Want To Enter: ");
+            scanf("%d", &data);
 
             rear++;
-            que[pointer] = data;
+            que[rear] = data;
+            front = 0;
 
+            // For Printing Updated Queue
             printf("Que After Inerting Data: \n");
-            for (pointer = 0; pointer < rear; pointer++)
+            for (i = front; i <= rear; i++)
             {
-                printf("%d ", que[pointer]);
+                printf("%d ", que[i]);
             }
         }
         else
             printf("Que is Owerflow");
         break;
     case 2:
-        printf("\n\nEnter a Data You Want To Delet: ");
-        if (rear != 0)
+        
+        if (front == -1)
         {
-
-            data = que[0];
-            printf("\nDeleted Data is %d", data);
-            printf("\nQue After Deleting Data: \n");
-            for (pointer = 1; pointer < rear; pointer++)
-            {
-                printf("%d ", que[pointer]);
-            }
+            printf("Que is Underflow");
         }
         else
-            printf("Que is Empty");
+        {
+            data = que[front];
+            if (front == rear)
+            {
+                front = -1;
+                rear = -1;
+            }
+            else
+            {
+                front++;
+            }
+
+            // For Printing Updated Queue
+            printf("Que After Inerting Data: \n");
+            for (i = front; i <= rear; i++)
+            {
+                printf("%d ", que[i]);
+            }
+        }
         break;
 
     default:
